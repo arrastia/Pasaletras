6e2fc5bb37a82df87980fb9585443cbbc57b45cd
Arrastia <arrastia@pm.me>
2020-04-22 19:56:50 +0200

    add Footer

---
+++ src/interfaces/.components/Layout/.components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+
+import styles from './Footer.module.scss';
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      {/* {language[translation.selected]['footer']}
+      <a className={styles.link} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        {language[translation.selected]['react']}
+      </a> */}
+    </footer>
+  );
+};
