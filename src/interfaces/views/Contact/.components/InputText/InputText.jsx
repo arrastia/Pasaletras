b21215001d9a2c1e7acb68e9a4691e4ec1844c04
Arrastia <arrastia@pm.me>
2020-05-09 20:20:20 +0200

    add InputText component

---
+++ src/interfaces/views/Contact/.components/InputText/InputText.jsx
@@ -0,0 +1,54 @@
+import React, { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+
+import classNames from 'classnames';
+
+import './InputText.scss';
+
+import { InputTextUtils } from './.tools/Utils/InputTextUtils';
+import DomHandler from 'interfaces/.tools/Utils/DomHandler';
+
+export const InputText = forwardRef(({ keyfilter, onInput, onKeyPress, validateOnly, ...rest }, ref) => {
+  const onPressKey = event => {
+    if (onKeyPress) onKeyPress(event);
+    if (keyfilter) InputTextUtils.onKeyPress(event, keyfilter, validateOnly);
+  };
+
+  const onInputClick = event => {
+    let validatePattern = true;
+    if (keyfilter && validateOnly) validatePattern = InputTextUtils.onValidate(event, keyfilter);
+    if (onInput) onInput(event, validatePattern);
+
+    if (!rest.onChange) {
+      if (event.target.value.length > 0) DomHandler.addClass(event.target, 'p-filled');
+      else DomHandler.removeClass(event.target, 'p-filled');
+    }
+  };
+
+  const classNameList = classNames('p-inputtext p-component', rest.className, {
+    'p-disabled': rest.disabled,
+    'p-filled': (rest.value && rest.value.toString().length > 0) || (rest.defaultValue && rest.defaultValue.toString().length > 0)
+  });
+
+  let inputProps = InputTextUtils.findDiffKeys(rest, InputText.defaultProps);
+
+  return <input ref={ref} {...inputProps} className={classNameList} onInput={onInputClick} onKeyPress={onPressKey} />;
+});
+
+InputText.propTypes = {
+  keyfilter: PropTypes.any,
+  onInput: PropTypes.func,
+  onKeyPress: PropTypes.func,
+  tooltip: PropTypes.string,
+  tooltipOptions: PropTypes.object,
+  validateOnly: PropTypes.bool
+};
+
+InputText.defaultProps = {
+  keyfilter: null,
+  onInput: null,
+  onKeyPress: null,
+  tooltip: null,
+  tooltipOptions: null,
+  validateOnly: false
+};
