bfd5de69114a5ace8658ce4251ea5745887934e5
Arrastia <arrastia@pm.me>
2020-05-12 18:47:10 +0200

    fix img size

--- src/interfaces/views/Author/Author.jsx
+++ src/interfaces/views/Author/Author.jsx
@@ -4,7 +4,7 @@ import styles from './Author.module.scss';
 
 import { GrLinkedin, GrLinkedinOption } from 'react-icons/gr';
 
-import author from 'assets/img/author.jpg';
+import author from 'assets/img/author2.jpg';
 
 import { SectionLayout } from 'interfaces/.components/SectionLayout';
 
--- src/interfaces/views/Home/Home.jsx
+++ src/interfaces/views/Home/Home.jsx
@@ -4,8 +4,8 @@ import { GiShoppingCart, GiTabletopPlayers } from 'react-icons/gi';
 
 import styles from './Home.module.scss';
 
-import book from 'assets/img/Pasaletras_mockup5.jpg';
-import girl from 'assets/img/Pasaletras_mockup7.jpg';
+import book from 'assets/img/fondo1.jpg';
+import girl from 'assets/img/fondo2.jpg';
 
 import { Slider } from './.components/Slider';
 
